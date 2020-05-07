6aca5054cecbb3f3fb313a2c64b02001e45ff648
Dragos-Mihai <dragos_mihai_prodan@yahoo.com>
2020-05-07 22:29:07 +0300

    update the test file

--- src/main/java/pizzashop/service/PizzaServiceTest.java
+++ src/main/java/pizzashop/service/PizzaServiceTest.java
@@ -56,7 +56,8 @@ class PizzaServiceTest {
 
         Double result=serviceEmpty.getTotalAmount(PaymentType.CARD);
         assertEquals(0,result);
-    }    @Test
+    }
+    @Test
     void getTotalAmount3_0() {
         service.addPayment(1,PaymentType.CASH,1.0);
         Double result=service.getTotalAmount(PaymentType.CASH);
@@ -68,63 +69,63 @@ class PizzaServiceTest {
         Double result=service.getTotalAmount(PaymentType.CARD);
         assertNotEquals(0,result);
     }
-//    @Disabled
-//    @DisplayName("ECPTest2")
-//    @org.junit.jupiter.api.Test
-//    void addPaymentecp1() {
-//        //service.addPayment("Asd", PaymentType.CASH,"Asd");
-//        //this test can not be run because have wrong parameters
-//        System.out.println("test nerulat");
-//    }
-//
-//
-//    @org.junit.jupiter.api.Test
-//    @DisplayName("ECPTest1")
-//    void addPaymentecp2() {
-//        int n=service.getPayments().size();
-//        service.addPayment(1,PaymentType.CASH,1.0);
-//        int m=service.getPayments().size();
-//        assertEquals(n+1,m);
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    @DisplayName("ECPTest3")
-//    void addPaymentecp3() {
-//
-//        assertThrows(IllegalArgumentException.class,()->{service.addPayment(10,PaymentType.CASH,-1.0);});
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    @DisplayName("bvaTest1")
-//    @ParameterizedTest
-//    @ValueSource(ints = {-1,0,1})
-//    void addPaymentecp4(int k) {
-//
-//            assertThrows(IllegalArgumentException.class,()->{service.addPayment(k,PaymentType.CASH,-1.0);});
-//
-//    }
-//    @org.junit.jupiter.api.Test
-//    @DisplayName("bvaTest2")
-// @RepeatedTest(3)
-//    void addPaymentecp5(RepetitionInfo repetitionInfo) {
-//        if(repetitionInfo.getCurrentRepetition()>=2){
-//        int n=service.getPayments().size();
-//        service.addPayment(1,PaymentType.CASH,-2+repetitionInfo.getCurrentRepetition());
-//        int m=service.getPayments().size();
-//        assertEquals(n+1,m);}
-//        else{
-//            assertThrows(IllegalArgumentException.class,()->{service.addPayment(1,PaymentType.CASH,-2+repetitionInfo.getCurrentRepetition());});
-//        }
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    @DisplayName("bvaTest3")
-//    @RepeatedTest(3)
-//    void addPaymentecp6(RepetitionInfo repetitionInfo) {
-//        int n=service.getPayments().size();
-//        service.addPayment(6+repetitionInfo.getCurrentRepetition(),PaymentType.CASH,-2+repetitionInfo.getCurrentRepetition()+Double.MAX_VALUE);
-//        int m=service.getPayments().size();
-//        assertEquals(n+1,m);
-//    }
+    @Disabled
+    @DisplayName("ECPTest2")
+    @org.junit.jupiter.api.Test
+    void addPaymentecp1() {
+        //service.addPayment("Asd", PaymentType.CASH,"Asd");
+        //this test can not be run because have wrong parameters
+        System.out.println("test nerulat");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("ECPTest1")
+    void addPaymentecp2() {
+        int n=service.getPayments().size();
+        service.addPayment(1,PaymentType.CASH,1.0);
+        int m=service.getPayments().size();
+        assertEquals(n+1,m);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("ECPTest3")
+    void addPaymentecp3() {
+
+        assertThrows(IllegalArgumentException.class,()->{service.addPayment(10,PaymentType.CASH,-1.0);});
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("bvaTest1")
+    @ParameterizedTest
+    @ValueSource(ints = {-1,0,1})
+    void addPaymentecp4(int k) {
+
+            assertThrows(IllegalArgumentException.class,()->{service.addPayment(k,PaymentType.CASH,-1.0);});
+
+    }
+    @org.junit.jupiter.api.Test
+    @DisplayName("bvaTest2")
+ @RepeatedTest(3)
+    void addPaymentecp5(RepetitionInfo repetitionInfo) {
+        if(repetitionInfo.getCurrentRepetition()>=2){
+        int n=service.getPayments().size();
+        service.addPayment(1,PaymentType.CASH,-2+repetitionInfo.getCurrentRepetition());
+        int m=service.getPayments().size();
+        assertEquals(n+1,m);}
+        else{
+            assertThrows(IllegalArgumentException.class,()->{service.addPayment(1,PaymentType.CASH,-2+repetitionInfo.getCurrentRepetition());});
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("bvaTest3")
+    @RepeatedTest(3)
+    void addPaymentecp6(RepetitionInfo repetitionInfo) {
+        int n=service.getPayments().size();
+        service.addPayment(6+repetitionInfo.getCurrentRepetition(),PaymentType.CASH,-2+repetitionInfo.getCurrentRepetition()+Double.MAX_VALUE);
+        int m=service.getPayments().size();
+        assertEquals(n+1,m);
+    }
 
 }
